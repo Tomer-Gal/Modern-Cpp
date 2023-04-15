ffecf86250c158149b92b03f8566b0bf0e26e680
Tomer Gal <Tomer.Gal@OpTeamizer.co.il>
2023-04-15 19:18:12 +0300

    Add Closure - capture by value

--- Course Material/FunctorAndLambda.cpp
+++ Course Material/FunctorAndLambda.cpp
@@ -34,3 +34,17 @@ TEST(sort_test, reverse_string_sort_lambda)
     // Check that the vector is sorted in reverse alphabetical order
     EXPECT_EQ(v, (std::vector<std::string>{"date", "cherry", "banana", "apple"}));
 }
+
+TEST(closure, capture_by_value)
+{
+    int x = 5;
+    auto f = [x]() {
+        return x;
+    };
+    x = 10;
+    int result = f();
+
+    // The value of x is captured by value, so the value of x in the closure
+    // is 5, even though x is changed to 10 after the closure is created.
+    EXPECT_EQ(result, 5);
+}
