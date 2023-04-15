ab09460e022526a09f22057ba81233509f861a6e
Tomer Gal <Tomer.Gal@OpTeamizer.co.il>
2023-04-15 23:26:21 +0300

    Add a friend example

--- Course Material/friend.cpp
+++ Course Material/friend.cpp
@@ -22,3 +22,40 @@ TEST(friend_tester, friend_test)
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_EQ(output, "The value of MyClass is: 123\n");
 }
+
+class One 
+{
+private:
+    int private_variable{};
+
+protected:
+    int protected_variable{};
+
+public:
+    One()
+    {
+        private_variable = 10;
+        protected_variable = 99;
+    }
+
+    // friend class declaration
+    friend class Two;
+};
+
+class Two 
+{
+public:
+    void display(One& t)
+    {
+        std::cout << "The value of Private Variable = " << t.private_variable << std::endl;
+        std::cout << "The value of Protected Variable = " << t.protected_variable;
+    }
+};
+
+TEST(friend_tester, friend_class_test)
+{
+    One t1;
+	Two t2;
+	t2.display(t1);
+
+}
